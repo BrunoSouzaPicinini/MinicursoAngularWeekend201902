79596abab9b2590d57ddfd14fd9b92464937b41d
BrunoSouzaPicinini <brunosouzapicinini@gmail.com>
2019-02-02 16:43:31 -0200

    correcao data bind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlanetaComponent} from './planeta/planeta.component';
+import { HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { PlanetaComponent} from './planeta/planeta.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
