f5cb781933f07f108dd55e3509f3367598792453
BrunoSouzaPicinini <brunosouzapicinini@gmail.com>
2019-02-02 14:20:28 -0200

    novo component planeta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlanetaComponent} from './planeta/planeta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/planeta/planeta.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'glx-planeta',
+    templateUrl: './planeta.component.html'
+})
+export class PlanetaComponent {
+    
+}
